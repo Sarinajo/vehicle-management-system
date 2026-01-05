faf24f92af3a2783a7fa6371fc119990a46c133e
Sarina Joshi <joshisarina19@gmail.com>
2026-01-05 14:40:33 +0545

    commit latest

--- vms/settings.py
+++ vms/settings.py
@@ -76,9 +76,10 @@ WSGI_APPLICATION = 'vms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'login'
